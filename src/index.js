422dbd3d2c0aa80dc7cd8eeff87e6677c4794d5c
nishant <nishant.20235@knit.ac.in>
2024-07-18 00:30:39 +0530

    completed basic sequelize setup for creating a db

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const { PORT } = require('./config/serverConfig');
+
 
 const setupAndStartServer = (async () => {
    const app = express();
+
+   app.use(bodyParser.json());
+   app.use(bodyParser.urlencoded({extended: true}));
+
    app.listen(PORT, ()=>{
     console.log(`Server running at ${PORT}`);
     // console.log(process.env);
